4713d06cac58756049601cb736d62d65d2d2317d
Dylan Brophy <dylanbrophy@gmail.com>
2020-01-29 19:16:10 -0800

    Update portforward.sh

--- vpn/portforward.sh
+++ vpn/portforward.sh
@@ -1,9 +1,7 @@
 if [ "$#" -eq 2 ]
 then
-	
-	
-	
-	
+	sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport $2 -j DNAT --to-destination $1:$2
+	sudo iptables -A FORWARD -p tcp -d $1 --dport $2 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 else
 	echo "Usage: $0 target-ipaddress portnumber"
 	exit 1
